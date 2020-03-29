7ced73c0f4efa77e986e6b2c484038519597739a
Arldeath <dima.markowitsch.02@mail.ru>
2020-03-29 14:22:00 +0300

    Вспомнил про PEP. Реализовал саму идею задания, немного интерактива и проверки на корректность запроса

--- lab_2/17_lab_2_1.py
+++ lab_2/17_lab_2_1.py
@@ -1,15 +1,13 @@
-keyslist = [
-    ["Сумм", "сумм"],
-    ["Максим", "максим"],
-    ["Миним", "миним"]]
+keyslist = ["умм", "аксим", "иним"]
 funclist = [sum, max, min]
-print("""Здравствуйте
+funcdict = dict(map(lambda *args: args, keyslist, funclist)) 
+print("""Здравствуйте!
 Рекомендация вводить запрос по типу:
-*Действие в последовательности данных в диапазоне*
+*Действие в последовательности данных в диапазоне.*
 Например:
 Максимум в массиве (2, 6, 90, -1, -23, 324, -3124, 5) с 3 элемента по 7.
 В противном случае вы можете получить не то, что нужно.
-Чтобы выйти из программы напишите 'q'
+Чтобы выйти из программы напишите 'q.'
 Введите ваш запрос:)""")
 request = ''
 while True:
@@ -17,6 +15,9 @@ while True:
     if request == 'q':
         break
     A = list(request)
+    if A[-1].isdigit():
+        A.append('.')
+        print("Фи, предложение без точки :(")
     b, counter = [], 0
     mylen, i = len(A), 0
     while i < mylen:
@@ -31,5 +32,43 @@ while True:
         if temp:
             b.append(''.join(temp))
         i += 1
-    counter = len(b)
-    print(b, counter)        
+    mylen = len(b)
+    i = 0
+    while i < mylen:
+        b[i]=int(b[i])
+        i += 1
+    begin, end, counter = b.pop(-2)-1, b.pop(-1)-1, len(b)
+    bool1 = end>begin and begin>0
+    if bool1 and counter>end:
+        usesfunc = None
+        for key in keyslist:
+            if request.find(key) + 1:
+                usesfunc = funcdict[key]
+        if usesfunc is None:
+            print("Ваш запрос некорректный, попробуйте ещё.")
+        else:
+            counter = end-begin+1
+            if counter % counter**0.5 == 0:
+                mylen = int(counter**0.5)
+                bool2 = False
+            else:
+                mylen = int(counter**0.5) + 1
+                bool2 = True
+            mylist = []
+            if bool2:
+                temp2 = end
+                end -= mylen
+                while begin < end:
+                    mylist.append(b[begin:begin+mylen])
+                    begin += mylen
+                mylist.append(b[begin:temp2])    
+            else:    
+                while begin < end:
+                    mylist.append(b[begin:begin+mylen])
+                    begin += mylen
+            temp = []
+            for elem in mylist:
+                temp.append(usesfunc(elem))
+            print(usesfunc(temp))
+    else:
+        print("Ваш запрос некорректный, попробуйте ещё.")
