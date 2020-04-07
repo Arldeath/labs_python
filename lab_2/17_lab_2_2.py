54083935378cefc02f77a2d70b4e11099065e6ec
Arldeath <dima.markowitsch.02@mail.ru>
2020-04-08 01:25:38 +0300

    Реализовал суть задания с помощью random. Реализовал интерактивное отображение статуса. Проверка иеперь работает более корректно и более менее оптимизирована. Код подогнан под PEP. (но это не точно)

--- lab_2/17_lab_2_2.py
+++ lab_2/17_lab_2_2.py
@@ -5,10 +5,10 @@ print(r"""Введите запрос по образцу:\n"
 request = input().split()
 len_of_request = len(request)
 if len_of_request == 6:
-    K = (request[2][1], request[3][0])
-    L = (request[4][1], request[5][0])
+    K = (int(request[2][1]), int(request[3][0]))
+    L = (int(request[4][1]), int(request[5][0]))
 elif len_of_request == 4:
-    K = (request[2][1], request[3][0])
+    K = (int(request[2][1]), int(request[3][0]))
     L = (3, 10)
 elif len_of_request == 2:
     K = (10, 100)
@@ -17,12 +17,32 @@ else:
     print("Вы ввели данные неккоретно")
     it_is_doing = False
 if it_is_doing:
-    Mb = request[1]
+    import random as rand
+    Mb = int(request[1])
+    begin_K, end_K = K[0], K[1]
+    begin_L, end_L = L[0], L[1]
     name = request[0]
-    new_file = open(name, "wb")
+    new_file = open(name, "w")
     size = 0
     need_size = Mb*1024*8
-    while size != need_size:
-        pass
+    counter_for_description = 0
+    while size < need_size:
+        counter_of_words = 0
+        number_of_words = rand.randint(begin_K, end_K)
+        while counter_of_words < number_of_words:
+            counter_of_chars = 0
+            number_of_chars = rand.randint(begin_L, end_L)
+            while counter_of_chars < number_of_chars:
+                new_file.write(chr(rand.randint(33, 127)))
+                size += 1
+                counter_of_chars += 1
+            new_file.write(' ')
+            counter_of_words += 1
+            size += 1
+        new_file.write('\n')
         size += 1
+        if (size/need_size)*100 > counter_for_description:
+            #print(counter_for_description, end='\r')
+            print(counter_for_description, end='\r')
+            counter_for_description += 1
     new_file.close()
