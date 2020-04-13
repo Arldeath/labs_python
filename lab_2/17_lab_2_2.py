f731e3df7b433431738a73be4a03b1b60b09187f
Arldeath <dima.markowitsch.02@mail.ru>
2020-04-13 23:22:04 +0300

    Реализовано заполнение именно буквами английского алфавита.

--- lab_2/17_lab_2_2.py
+++ lab_2/17_lab_2_2.py
@@ -1,14 +1,16 @@
+alphabet_keys = list(range(65, 91)) + list(range(97, 123))
+
 it_is_doing = True
-print(r"""Введите запрос по образцу:\n"
-<имя> <размер в Мб> <диапазон кол-ва строк в строке> <длина слова>\n"
+print("""Введите запрос по образцу:
+<имя> <размер в Мб> <диапазон кол-ва строк в строке> <длина слова>
 Наппример, *testname.txt 512 (20, 50) (5, 9)*""")
 request = input().split()
 len_of_request = len(request)
 if len_of_request == 6:
-    K = (int(request[2][1]), int(request[3][0]))
-    L = (int(request[4][1]), int(request[5][0]))
+    K = (int(request[2][1:-1]), int(request[3][0:-1]))
+    L = (int(request[4][1:-1]), int(request[5][0:-1]))
 elif len_of_request == 4:
-    K = (int(request[2][1]), int(request[3][0]))
+    K = (int(request[2][1:-1]), int(request[3][0:-1]))
     L = (3, 10)
 elif len_of_request == 2:
     K = (10, 100)
@@ -24,7 +26,7 @@ if it_is_doing:
     name = request[0]
     new_file = open(name, "w")
     size = 0
-    need_size = Mb*1024*8
+    need_size = Mb*1024*1024
     counter_for_description = 0
     while size < need_size:
         counter_of_words = 0
@@ -33,7 +35,7 @@ if it_is_doing:
             counter_of_chars = 0
             number_of_chars = rand.randint(begin_L, end_L)
             while counter_of_chars < number_of_chars:
-                new_file.write(chr(rand.randint(33, 127)))
+                new_file.write(chr(rand.choice(alphabet_keys)))
                 size += 1
                 counter_of_chars += 1
             new_file.write(' ')
@@ -42,7 +44,6 @@ if it_is_doing:
         new_file.write('\n')
         size += 1
         if (size/need_size)*100 > counter_for_description:
-            #print(counter_for_description, end='\r')
             print(counter_for_description, end='\r')
             counter_for_description += 1
     new_file.close()
